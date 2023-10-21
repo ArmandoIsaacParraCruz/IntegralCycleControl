422c9b75708ee6b3d59f3dba542e210f49d41b1f
ArmandoIsaacParraCruz <armandoisaac.parracruz@gmail.com>
2023-10-21 13:04:10 -0600

    adding debounce time

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,20 @@
 
 void zeroCrossingInterrupt();
 
-const int zeroCrossingPin = 2; 
+const uint8_t ZERO_CROSSING_PIN = 2;
+const uint16_t SEMICYCLE_PERIOD = 8000; 
 
 uint32_t lastTime;
+uint32_t lastZeroCrossingActivationTime;
 
 volatile uint32_t SemicyclesCounter = 0;  
 
 void setup() {
-  pinMode(zeroCrossingPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(zeroCrossingPin), zeroCrossingInterrupt, RISING);
+  pinMode(ZERO_CROSSING_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ZERO_CROSSING_PIN), zeroCrossingInterrupt, RISING);
   Serial.begin(9600);
 	lastTime = millis();
+	lastZeroCrossingActivationTime = micros();
 }
 
 void loop() {
@@ -25,6 +28,9 @@ void loop() {
 }
 
 void zeroCrossingInterrupt() {
-  ++SemicyclesCounter;
+	if(micros() - lastZeroCrossingActivationTime > SEMICYCLE_PERIOD) {
+		++SemicyclesCounter;
+		lastZeroCrossingActivationTime = micros();
+	}
 }
 
