6c0f7d15c41f86aa2764094c46afbb9902b21ed2
ArmandoIsaacParraCruz <armandoisaac.parracruz@gmail.com>
2023-11-14 19:22:48 -0600

    SPI communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,58 @@
 #include <Arduino.h>
+#include <SPI.h>
+
+const uint8_t ssPin = 10; 
+const uint16_t SEMICYCLE_PERIOD = 8000;
+const uint8_t NUMBER_OF_HEATING_RESISTORS = 6;
+const uint8_t HEATING_RESISTOR_PINS[NUMBER_OF_HEATING_RESISTORS] = {A0,A1,A2,A3,A4,A5};
+const uint8_t ZERO_CROSSING_PIN = 2;
+uint8_t semicyclesPerHeatingResistor[NUMBER_OF_HEATING_RESISTORS];
+uint8_t arrayIndex;
+uint32_t lastTime;
+uint32_t lastZeroCrossingActivationTime;
+volatile uint8_t option;
+volatile uint32_t SemicyclesCounter = 0;  
+
+void setup() {
+	SPCR |= _BV(SPE);
+	pinMode(SS, INPUT);
+	pinMode(MISO, OUTPUT);
+	SPI.attachInterrupt();
+	arrayIndex = 0;
+
+	for(uint8_t i = 0; i < NUMBER_OF_HEATING_RESISTORS; ++i) {
+		pinMode(HEATING_RESISTOR_PINS[i], OUTPUT);
+		digitalWrite(HEATING_RESISTOR_PINS[i], HIGH);
+		semicyclesPerHeatingResistor[i] = 0;
+	}
+	lastTime = millis();
+}
+
+void loop() {
+	
+	for(uint8_t i = 0; i < NUMBER_OF_HEATING_RESISTORS; ++i) {
+		if(semicyclesPerHeatingResistor[i] >= 1) {
+			digitalWrite(HEATING_RESISTOR_PINS[i], HIGH);
+		} else {
+			digitalWrite(HEATING_RESISTOR_PINS[i], LOW);
+		}
+	}
+}
+
+ISR(SPI_STC_vect) {
+  	uint8_t semicycles = SPDR;
+	semicyclesPerHeatingResistor[arrayIndex] = semicycles;
+	++arrayIndex;
+	if(arrayIndex >= NUMBER_OF_HEATING_RESISTORS) {
+		arrayIndex = 0;
+	}
+}
+
+
+
+
+
+/*
 
 void zeroCrossingInterrupt();
 void semicycleIntegralHeatingPowerAdjustment();
@@ -54,3 +108,7 @@ void semicycleIntegralHeatingPowerAdjustment(){
 	}
 }
 
+*/
+
+
+  
